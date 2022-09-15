a15d76e530321326f2b92190d4bad944d32204ef
your name”git add .git config --global user.name ridwan <104872709+ridwanmuse@users.noreply.github.com>
2022-09-15 16:50:16 -0500

    “done”

--- client/profile.js
+++ client/profile.js
@@ -1,20 +1,30 @@
-const happinessbtn= document.querySelector("#happinessbtn");
-
-const baseURL= "http://localhost:3000/api/"
-
-const message= document.querySelector("#message")
-
 function happiness(){
+    alert("do something just because it makes you feel good, be who you want to be, do the things that give your life more meaning") }
     axios.get(`${baseURL}happiness`)
     .then(res => message.innerHTML = res.data.input)
-    .catch(err => console.log(err.data))} 
-happinessbtn.addEventListener("click",happiness)
+happiness.addEventListener("click", happiness);
+
+function sadness(){
+    alert("you may feel small, but to someone, you are big")}
+sadness.addEventListener("click", sadness);
+
+function anger(){
+    alert("Being angry is okay. Being upset is okay. Feeling your emotions is okay. You do not have to have permission to feel things.")}
+sadness.addEventListener("click", sadness);
+
+function disgust(){
+    alert("Don't feel disgusted with yourself.")}
+sadness.addEventListener("click", sadness);
 
 
+function fear(){
+    alert("I don't need to be afraid of change. everything is always moving, flowing, shifting.")}
+sadness.addEventListener("click", fear);
 
 
-
-
+function surprise(){
+    alert("Surprising oneself is the greatest form of surprise.")}
+sadness.addEventListener("click", sadness);
 
 
 
